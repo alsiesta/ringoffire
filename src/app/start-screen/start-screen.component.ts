6054f50e2daebf12034e2354dbc27d416256e04c
alsiesta <as@daslizenzatelier.de>
2023-03-07 15:36:19 +0100

    animated cards

---
+++ src/app/start-screen/start-screen.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-start-screen',
+  templateUrl: './start-screen.component.html',
+  styleUrls: ['./start-screen.component.scss']
+})
+export class StartScreenComponent implements OnInit {
+  ngOnInit(): void { }
+  
+  constructor(private router: Router) { }
+  
+  newGame() {
+    this.router.navigateByUrl('/game');
+}
+}
