f0d8fed9f9befeb340cc5061b8529613399b4011
alsiesta <as@daslizenzatelier.de>
2023-03-08 01:00:31 +0100

    play card and show

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -4,22 +4,34 @@ import { Game } from 'src/models/game';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
   pickCardAnimation = false;
+  currentCard: string = '';
   game: Game;
-  constructor() { }
-  
-  ngOnInit(): void { 
+  constructor() {}
+
+  ngOnInit(): void {
     this.newGame();
   }
 
   newGame() {
     this.game = new Game();
     console.log(this.game);
-}
+  }
   takeCard() {
-    this.pickCardAnimation = true;
+    if (!this.pickCardAnimation) {
+      this.currentCard = this.game.stack.pop();
+      // console.log(this.currentCard);
+      this.pickCardAnimation = true;
+      // console.log('cards remaining in stack: ', this.game.stack)
+      // console.log('played cards are: ', this.game.playedCards)
+      
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }
